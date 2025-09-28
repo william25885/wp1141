74e4e118955819a71070848d5ae41a044f43a58e
william25885 <william25885@gmail.com>
2025-09-29 05:56:41 +0800

    Create useGameLoop.ts

---
+++ hw2/game/src/hooks/useGameLoop.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react';
+
+export const useGameLoop = (callback: () => void, isActive: boolean) => {
+  const requestRef = useRef<number | undefined>(undefined);
+  const previousTimeRef = useRef<number | undefined>(undefined);
+
+  const animate = (time: number) => {
+    if (previousTimeRef.current !== undefined) {
+      callback();
+    }
+    previousTimeRef.current = time;
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    if (isActive) {
+      requestRef.current = requestAnimationFrame(animate);
+    } else {
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current);
+      }
+    }
+
+    return () => {
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current);
+      }
+    };
+  }, [isActive, callback]);
+};
